e7ec8de627c8c78ccdafaff183b1ba2bb05a367a
Kevin Mader <kmader@users.noreply.github.com>
2018-08-28 17:17:04 +0200

    Create setup.py
    first very simple setup.y

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+
+setup(name='jupyanno',
+      version='0.0.1',
+      description='Easy for doing radiology in python',
+      long_description=open('README.md').read(),
+      url='https://www.github.com/chestrays/jupyanno',
+      license='Apache',
+      author='ChestRays',
+      packages=['jupyanno'],
+      install_requires=['numpy', 'pandas', 'notebook', 'ipywidgets'],
+      extras_require={
+        'plots':  ["matplotlib", "plotly"]
+        }
+      )
