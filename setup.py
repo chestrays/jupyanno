934941207b36c9ea21a9eaa6e31ab783ada952c5
Kevin Mader <kmader@magicleap.com>
2019-09-19 10:57:22 +0200

    replacing the apps/appmode with voila

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(name='jupyanno',
       author='ChestRays Team',
       packages=['jupyanno'],
       install_requires=['numpy', 'pandas', 'notebook', 'ipywidgets',
-                        'seaborn', 'pillow', 'beautifulsoup4', 'pydicom'],
+                        'seaborn', 'pillow', 'beautifulsoup4', 'pydicom',
+                        'requests'],
       extras_require={
           'plots': ["matplotlib", "plotly", "cornerstone_widget"]
       }
